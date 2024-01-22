177f301a5077cdf0507177df6ed16b6cb0f65745
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2024-01-22 10:17:57 +0300

    to ExecutorService

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,57 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public ThreadPoolTaskExecutor customTaskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor() {
-			@Override
-			public void shutdown() {
-				super.shutdown();
-				System.out.println("customShutdown");
-			}
-		};
-		executor.setMaxPoolSize(1);
-		executor.setThreadNamePrefix("customThread-");
-		return executor;
+    @Bean
+    public ExecutorService customTaskExecutor() {
+        return new ThreadPoolExecutor(1, 1, 60L, TimeUnit.SECONDS, new SynchronousQueue<>()) {
+            @Override
+            public void shutdown() {
+                super.shutdown();
+                System.out.println("customShutdown");
+            }
+        };
+    }
 
-	}
+    @RestController
+    @Slf4j
+    public static class DemoController {
 
-	@RestController
-	@Slf4j
-	public static class DemoController {
+        @Autowired
+        private ExecutorService customTaskExecutor;
 
-		@Autowired
-		private ThreadPoolTaskExecutor customTaskExecutor;
-
-		@GetMapping("test")
-		@SneakyThrows
-		public String get() {
-			Future<String> future = customTaskExecutor.submit(() -> {
-				log.warn("Execute start method - " + Thread.currentThread().getName());
-				Thread.sleep(5_000);
-				log.warn("Execute end method - " + Thread.currentThread().getName());
-				return "test";
-			});
-			return future.get();
-		}
-	}
+        @GetMapping("test")
+        @SneakyThrows
+        public String get() {
+            Future<String> future = customTaskExecutor.submit(() -> {
+                log.warn("Execute start method - " + Thread.currentThread().getName());
+                Thread.sleep(5_000);
+                log.warn("Execute end method - " + Thread.currentThread().getName());
+                return "test";
+            });
+            return future.get();
+        }
+    }
 }
